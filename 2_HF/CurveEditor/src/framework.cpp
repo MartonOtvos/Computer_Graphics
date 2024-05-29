af45400bc49f9923a48228d0c85dd252220b51b0
Ötvös Márton Álmos <otvos.marton.almos@gmail.com>
2024-05-29 18:46:57 +0200

    Rename Window

--- 2_HF/CurveEditor/src/framework.cpp
+++ 2_HF/CurveEditor/src/framework.cpp
@@ -25,7 +25,7 @@ int main(int argc, char * argv[]) {
 #else
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 #endif
-	glutCreateWindow("Line Manipulation");
+	glutCreateWindow("Curve Editor");
 
 #if !defined(__APPLE__)
 	glewExperimental = true;	// magic
